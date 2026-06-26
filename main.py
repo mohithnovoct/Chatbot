afb259333ce3a7c79a915f213634ebd67fc76471
Mohith Butta <mohithbutta4002@gmail.com>
2026-06-26 15:19:31 +0530

    defined a working api endpoint

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from chatbot!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from langchain_core.messages import HumanMessage
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(title="Simple Chatbot")
+
+llm = ChatGoogleGenerativeAI(model="gemma-4-31b-it", google_api_key=os.getenv("GEMINI_API_KEY"), temperature=0.6)
+
+class TopicRequest(BaseModel):
+    topic: str
+
+@app.post("/explain")
+def explain_topic(request: TopicRequest):
+    prompt = f"Explain this topic in simple terms: {request.topic}"
+    response = llm.invoke([HumanMessage(content=prompt)])
+    return {"topic": request.topic, "explanation": response.content}
